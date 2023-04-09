fbde4cca1bf010d1d224220b964a46df84fd72c9
joaozati <jzati@mitsidi.com>
2023-04-08 21:17:03 -0300

    Adicionando search bar  no form

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
 import Form from 'react-bootstrap/Form';
 
-function FormTextExample() {
-    return (
-        <>
-            <Form.Label htmlFor="inputPassword5">
-                Search Bar
-            </Form.Label>
-            <Form.Control
-                type="password"
-                id="inputPassword5"
-                aria-describedby="passwordHelpBlock"
-                placeholder="Search for new image here..."
-            />
-            {/* <Form.Text id="passwordHelpBlock" muted>
-                Your password must be 8-20 characters long, contain letters and numbers,
-                and must not contain spaces, special characters, or emoji.
-            </Form.Text> */}
-        </>
-    );
+const searchStyle = {
+    width: '90%'
+}
+
+
+class CustomForm extends Component {
+    render() {
+        return (
+            <Form onSubmit={alert('Form submitted!')} id="image-form">
+                <Form.Label htmlFor="inputPassword5">
+                    Search Bar
+                </Form.Label>
+                <Form.Control
+                    type="password"
+                    id="inputPassword5"
+                    aria-describedby="passwordHelpBlock"
+                    placeholder="Search for new image here..."
+                />
+                {/* <Form.Text id="passwordHelpBlock" muted>
+                    Your password must be 8-20 characters long, contain letters and numbers,
+                    and must not contain spaces, special characters, or emoji.
+                </Form.Text> */}
+            </Form>
+        );
+    }
 }
 
 function Search() {
@@ -28,10 +35,10 @@ function Search() {
         <Container style = {{ marginTop: '2em'}}>
             <Row className="align-items-end">
                 <Col xs={8}>
-                    {FormTextExample()}
+                    <CustomForm />
                 </Col>
             <Col xs={4} >
-                    <Button variant="info" style={{ width: '90%' }}>Info</Button>{' '}
+                    <Button form="image-form" type="submit" variant="info" style={searchStyle}>Search</Button>{' '}
                 </Col>
             </Row>
         </Container>

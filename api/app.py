72423a2380c06423fe194d782ebf1a339b091f0b
joaozati <jzati@mitsidi.com>
2023-04-10 17:53:27 -0300

    Create new-image endpoint with dcouple for use .env in UNSPLASH_ACCESS_KEY

--- api/app.py
+++ api/app.py
@@ -1,15 +1,43 @@
 from flask import Flask, jsonify, request
+import requests
+import os
+from decouple import config
 
 app = Flask(__name__)
+
+UNSPLASH_ACCESS_KEY = config("UNSPLASH_ACCESS_KEY")
+UNSPLASH_URL = "https://api.unsplash.com"
+
+SEARCH_PHOTOS = "/search/photos"
+RANDOM_PHOTO = "/photos/random"
+
+if not UNSPLASH_ACCESS_KEY:
+    raise EnvironmentError("Please create .env with UNSPLASH_ACCES_KEY=<YOUR KEY>")
 
 
 @app.route('/new-image')
 def new_image():
+    url_photos = UNSPLASH_URL + RANDOM_PHOTO
     word = request.args.get("query")
 
+    headers = {
+        "Accept-Version": "v1",
+        "Authorization": f"Client-ID {UNSPLASH_ACCESS_KEY}"
+    }
 
-    response = {'word': word}
-    return jsonify(response)
+    params = {
+        "query": word
+    }
+
+    response = requests.get(
+        url=url_photos,
+        headers=headers,
+        params=params
+    )
+
+    data = response.json()
+    return data
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)

4a66a3fbada47261b78d73a0340bb5d69bdb6bd9
joaozati <jzati@mitsidi.com>
2023-04-10 16:07:33 -0300

    Initializing a simple flask app with pipfile

---
+++ api/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/api/hello')
+def hello():
+    response = {'message': 'Hello, World!'}
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run()

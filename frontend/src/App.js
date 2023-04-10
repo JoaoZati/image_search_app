a9f82a7c7d4e70f411c46d84f6e611c20f52ec01
joaozati <jzati@mitsidi.com>
2023-04-10 09:20:15 -0300

    Adicionando form para o botao de search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import { useState } from 'react';
 
-const handleSearchSubmit = (e) => {
-  e.preventDefault();
-  console.log(e.target[0].value);
-}
+
 
 function App() {
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
+  console.log('onchange: ' + word)
+
   return (
     <div>
       <Header title="Image Gallery" bg="info"/>
-      <Search handleSubmit={handleSearchSubmit}/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -19,6 +19,8 @@ class CustomForm extends Component {
                     id="inputPassword5"
                     aria-describedby="passwordHelpBlock"
                     placeholder="Search for new image here..."
+                    value={this.props.word}
+                    onChange={(e) => this.props.setWord(e.target.value)}
                 />
                 {/* <Form.Text id="passwordHelpBlock" muted>
                     Your password must be 8-20 characters long, contain letters and numbers,
@@ -30,12 +32,12 @@ class CustomForm extends Component {
 }
 
 
-function Search({handleSubmit}) {
+function Search({ word, setWord, handleSubmit}) {
     return (
         <Container style = {{ marginTop: '2em'}}>
             <Row className="align-items-end">
                 <Col xs={8}>
-                    <CustomForm onSubmit={handleSubmit}/>
+                    <CustomForm word={word} setWord={setWord} onSubmit={handleSubmit}/>
                 </Col>
             <Col xs={4} >
                     <Button form="image-form" type="submit" variant="info" style={searchStyle}>Search</Button>{' '}
